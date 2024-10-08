7cdb5df368f9902bef366d31f03e08ae3a11d586
PeterRanario <peterranario11@gmail.com>
2024-10-08 13:35:44 +0800

    Initial commit on Access Modifiers and Encapsulation

---
+++ Modifiers_Encapsulation.js
@@ -0,0 +1,17 @@
+class Person {
+
+    //Data fields
+    #name;
+    #age;
+    #gender;
+    #height;
+    #skill;
+
+    constructor(name, age, gender, height, skill) {
+        this.#name = name;
+        this.#age = age;
+        this.#gender = gender;
+        this.#height = height;
+        this.#skill = skill;
+    }
+}
