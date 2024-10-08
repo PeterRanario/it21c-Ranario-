ddb441c705d4b6cec512629bf8fdccbffc39aef5
PeterRanario <peterranario11@gmail.com>
2024-10-08 15:55:15 +0800

    Added method to display output via console and html.

--- Inheritance.js
+++ Inheritance.js
@@ -31,4 +31,24 @@ class Person {
         return this.#nickname;
     }
 
+     //Method that Displays output via console.log
+     displayInfo() {
+        console.log("Name: " + this.name);
+        console.log("Age: " + this.age);
+        console.log("Gender: " + this.gender);
+        console.log("Nickname: " + this.nickname);
+
+    }
+
+    //Method that Displays output via HTML
+    displayInfoViaHTML() {
+        const personElement = document.getElementById('person1');
+        personElement.innerHTML = `
+          <strong>Name: </strong> ${this.name} <br>
+          <strong>Age: </strong> ${this.age} <br>
+          <strong>Age: </strong> ${this.gender} <br>
+          <strong>Gender: </strong> ${this.nickname} 
+        `;
+    }
+
 }
