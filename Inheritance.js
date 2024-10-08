03b7db302ae3d051755eaafdbce49545c80cb3bc
PeterRanario <peterranario11@gmail.com>
2024-10-08 14:23:31 +0800

    Initial commit on Inheritance with data fields and constructor

---
+++ Inheritance.js
@@ -0,0 +1,18 @@
+class Person {
+
+    //Private Data Fields
+    #name;
+    #age;
+    #gender;
+    #nickname;
+
+    //constructor with parameters
+    constructor(name, age, gender, nickname) {
+    this.#name = name;
+    this.#age = age;
+    this.#gender = gender;
+    this.nickname = nickname;
+    }
+
+    
+}
