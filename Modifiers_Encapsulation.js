e861f0896f23f111e8991ffafa240bab2028eca5
PeterRanario <peterranario11@gmail.com>
2024-10-08 13:39:25 +0800

    Added Getters to return the data field

--- Modifiers_Encapsulation.js
+++ Modifiers_Encapsulation.js
@@ -7,12 +7,29 @@ class Person {
     #height;
     #skill;
 
-    
+
     constructor(name, age, gender, height, skill) {
         this.#name = name;
         this.#age = age;
         this.#gender = gender;
         this.#height = height;
         this.#skill = skill;
+    }
+
+    //getters to return the private data field
+    get name() {
+        return this.#name;
+    }
+    get age() {
+        return this.#age;
+    }
+    get gender() {
+        return this.#gender;
+    }
+    get height() {
+        return this.#height;
+    }
+    get weight() {
+        return this.#skill;
     }
 }
