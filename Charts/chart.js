5f8c89289b5728d97f24d9ddd5562fdc462aa817
PeterRanario <peterranario11@gmail.com>
2024-10-16 13:51:01 +0800

    added new methods

--- Charts/chart.js
+++ Charts/chart.js
@@ -3,4 +3,24 @@ class ChartCreator {
         this.dataUrl = dataUrl;
         this.chartData = null;
     }
+
+    async init() {
+        await this.fetchData();
+        if (this.chartData) {
+            this.createCharts();
+        }
+    }
+  
+    async fetchData() {
+        try {
+            const response = await fetch(this.dataUrl);
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            this.chartData = await response.json();
+        } catch (error) {
+            console.error('There has been a problem with your fetch operation:', error);
+        }
+    }
 }
+
