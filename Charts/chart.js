f1dc597798204f06dca9097b6c3ebcb38f0e0a4b
PeterRanario <peterranario11@gmail.com>
2024-10-16 13:57:04 +0800

    added constant with console

--- Charts/chart.js
+++ Charts/chart.js
@@ -63,3 +63,8 @@ class LineChart extends ChartCreator {
         });
     }
 }
+
+const lineChartCreator = new LineChart('data.json');
+lineChartCreator.init();
+
+console.log(lineChartCreator.dataUrl);
